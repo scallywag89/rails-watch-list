f5362b00fc6b2466d8b0da95f1024d9ff13d9c08
bennettmackenzie <bamsubscriptions@gmail.com>
2022-06-11 17:01:39 +1000

    trying to figure out the API seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+require 'open-uri'
+require 'nokogiri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
