e088c77fbc912e832b046adac48e51d99b65597e
bennettmackenzie <bamsubscriptions@gmail.com>
2022-06-13 11:01:11 +1000

    routed correctly to lists#index as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists do
+  root to: 'lists#index'
+
+  resources :lists, except: [:index] do
     resources :bookmarks
   end
   resources :bookmarks, only: [ :new, :create, :destroy ]
